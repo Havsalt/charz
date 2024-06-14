a829bf2b6754e6c520540102a0819134bf7bd8df
Havsalt <knutolaihelgesen@gmail.com>
2024-06-14 12:30:17 +0200

    Added more structure to class

--- charz/_node.py
+++ charz/_node.py
@@ -1,6 +1,24 @@
 from __future__ import annotations as _annotations
 
+from typing import Generator as _Generator
+
 
 class Node:
     uid: int
     parent: Node | None = None
+    _instances: dict[int, Node] = {}
+    _uid_counter = 0
+
+    def __init__(self) -> None:
+        self.uid = Node._uid_counter
+        Node._uid_counter += 1
+
+    @classmethod
+    def iter_nodes(cls) -> _Generator[Node, None, None]:
+        yield from Node._instances.values()
+
+    def setup(self) -> None:
+        ...
+    
+    def update(self, delta: float) -> None:
+        ...
