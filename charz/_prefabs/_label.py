2f5252dde7e035e0420075212d8fadd174dbb319
Havsalt <knutolaihelgesen@gmail.com>
2024-06-26 13:21:54 +0200

    Added more settings for Label

--- charz/_prefabs/_label.py
+++ charz/_prefabs/_label.py
@@ -7,14 +7,37 @@ from .._color import Color as _Color
 
 
 class Label(_Color, _Texture, _Transform, _Node):
+    newline: str = "\n"
+    tab_size: int = 4
+    tab_char: str = "\t"
+    tab_fill: str = " "
+
+    def with_newline(self, newline: str, /):
+        self.newline = newline
+        return self
+    
+    def with_tab_size(self, tab_size: int, /):
+        self.tab_size = tab_size
+        return self
+    
+    def with_tab_char(self, tab_char: str, /):
+        self.tab_char = tab_char
+        return self
+    
+    def with_tab_fill(self, tab_fill: str, /):
+        self.tab_fill = tab_fill
+        return self
+
     def with_text(self, text: str, /):
         self.text = text
         return self
 
     @property
     def text(self) -> str:
-        return "\n".join(self.texture)
+        joined_lines = self.newline.join(self.texture)
+        return joined_lines.replace(self.tab_fill * self.tab_size, self.tab_char)
     
     @text.setter
     def text(self, value: str) -> None:
-        self.texture = value.split("\n")
+        tab_replaced = self.newline.replace(self.tab_char, self.tab_fill * self.tab_size)
+        self.texture = value.split(tab_replaced)
