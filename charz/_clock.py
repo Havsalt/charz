8f1b6a46084979de21da76928f76b646812307d5
Havsalt <knutolaihelgesen@gmail.com>
2024-09-29 12:11:43 +0200

    Removed with_* block

--- charz/_clock.py
+++ charz/_clock.py
@@ -2,9 +2,9 @@ from __future__ import annotations as _annotations
 
 import time as _time
 
-from typing_extensions import Self as _Self
 
-
+# NOTE: this class is not a `Node` subclass,
+#       and is therefore treated more like a datastructure with methods
 class Clock:
     """`Clock` base class, without delta time"""
 
@@ -16,11 +16,6 @@ class Clock:
         """
         self.tps = tps
         self._delta_time = 1.0 / tps  # average delta time
-
-    def with_tps(self, tps: float, /) -> _Self:
-        self.tps = tps
-        self._delta_time = 1.0 / tps  # NOTE: resets if started
-        return self
 
     @property
     def tps(self) -> float:
