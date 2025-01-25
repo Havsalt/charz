b98226fd7afa48207ce3407301af8a0c2e2cde3a
Havsalt <knutolaihelgesen@gmail.com>
2025-01-25 15:01:34 +0100

    Changed parent class from IntEnum -> Flag

--- charz/_camera.py
+++ charz/_camera.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from enum import IntEnum, auto
+from enum import Flag, auto
 from typing import ClassVar
 
 from typing_extensions import Self
@@ -9,32 +9,10 @@ from ._node import Node
 from ._components._transform import Transform
 
 
-class CameraMode(IntEnum):
-    FIXED = 0
+class CameraMode(Flag):
+    FIXED = auto()
     CENTERED = auto()
     INCLUDE_SIZE = auto()
-
-    def __or__(self, other: CameraMode) -> CameraMode:
-        """Merge two CameraMode values into a new CameraMode value
-
-        NOTE: Result may not be a member of `CameraMode`, but instead an `int`.
-        Combine any `CameraMode`, where any value that is not bound to a variant,
-        will in fact be an `int`, but we treat it like a `CameraMode` that was combined
-
-        Args:
-            other (CameraMode): other CameraMode value to merge with
-
-        Returns:
-            CameraMode: new combined CameraMode value
-        """
-        if isinstance(other, CameraMode):
-            new_value = self.value | other.value
-            if new_value in CameraMode._value2member_map_:
-                return CameraMode(new_value)
-            return new_value  # type: ignore
-        raise TypeError(
-            f"unsupported operand type(s) for |: 'CameraMode' and '{type(other)}'"
-        )
 
 
 class Camera(Transform, Node):
