e4bfd51ae6a0355d4ea7d4557ef1b5df8070cce8
Havsalt <77575424+Havsalt@users.noreply.github.com>
2025-05-05 18:41:48 +0200

    Updated core group namespace

--- charz/_grouping.py
+++ charz/_grouping.py
@@ -1,12 +1,12 @@
 from enum import Enum, unique
 
-from charz_core import CoreGroup
+import charz_core
 
 
 # TODO: Use `StrEnum` for Python 3.11+
 @unique
 class Group(str, Enum):
     # NOTE: variants in this enum produces the same hash as if it was using normal `str`
-    NODE = CoreGroup.NODE
+    NODE = charz_core.Group.NODE
     TEXTURE = "texture"
     COLLIDER = "collider"
