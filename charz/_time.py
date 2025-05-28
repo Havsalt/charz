9773b29ceac20eb7d7132375fafa62f661e68513
Havsalt <77575424+Havsalt@users.noreply.github.com>
2025-05-28 15:14:19 +0200

    Removed redundant metaclass

--- charz/_time.py
+++ charz/_time.py
@@ -5,19 +5,14 @@ from typing import NoReturn, Any, final
 from ._non_negative import NonNegative
 
 
-class TimeClassProperties(type):
-    delta = NonNegative[float](0)
-
-
 @final
-class Time(metaclass=TimeClassProperties):
+class Time:
     """`Time` is a class namespace used to store delta time
 
     `Time.delta` is computed by `Clock`, handled by `Engine` frame task,
     usually used in `Node.update`
     """
+    delta = NonNegative[float](0)
 
-    # Prevent instantiating,
-    # as this class only has class methods and class variables
     def __new__(cls, *_args: Any, **_kwargs: Any) -> NoReturn:
         raise RuntimeError(f"{cls.__name__} cannot be instantiated")
