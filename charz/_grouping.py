5f258198d131dcb20d4379cfd8fbcbaf6ba16d59
Havsalt <77575424+Havsalt@users.noreply.github.com>
2025-05-28 14:05:58 +0200

    Using StrEnum for python 3.11+

--- charz/_grouping.py
+++ charz/_grouping.py
@@ -1,15 +1,29 @@
 from __future__ import annotations
 
-from enum import Enum, unique
+import sys
+from enum import unique
 
 import charz_core
 
 
 # TODO: Use `StrEnum` for Python 3.11+
-@unique
-class Group(str, Enum):
-    # NOTE: Variants in this enum produces the same hash as if it was using normal `str`
-    NODE = charz_core.Group.NODE.value
-    TEXTURE = "texture"
-    ANIMATED = "animated"
-    COLLIDER = "collider"
+# NOTE: Variants of the enum produces the same hash as if it was using normal `str`
+if sys.version_info >= (3, 11):
+    from enum import StrEnum, auto
+
+    @unique
+    class Group(StrEnum):
+        NODE = charz_core.Group.NODE
+        TEXTURE = auto()
+        ANIMATED = auto()
+        COLLIDER = auto()
+
+else:
+    from enum import Enum
+
+    @unique
+    class Group(str, Enum):
+        NODE = charz_core.Group.NODE.value
+        TEXTURE = "texture"
+        ANIMATED = "animated"
+        COLLIDER = "collider"
